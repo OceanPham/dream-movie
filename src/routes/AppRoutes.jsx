5b8d8c35db7b4b01a4191c7d1dcff4f33ae5da92
Ocean <phamngochuy0102@gmail.com>
2024-09-23 21:57:55 +0700

    Create form add producer

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -12,6 +12,7 @@ import Logout from "../Auth/Logout";
 import SignUp from "../Auth/SignUp";
 import ExportTicket from "../views/pages/Dashboard/ExportTicket";
 import ChairCategory from "../views/pages/Managers/ChairCategory";
+import Producer from "../views/pages/Managers/Producer";
 
 const AppRouter = () => {
   return (
@@ -57,19 +58,39 @@ const AppRouter = () => {
       <Route
         path="/manager/chairCategory"
         element={
-          // <PrivateRoute>
-          <ChairCategory />
-          // </PrivateRoute>
+          <PrivateRoute>
+            <ChairCategory />
+          </PrivateRoute>
         }
       />
       <Route
         path="/manager/chairCategory/:alias"
         element={
-          // <PrivateRoute>
-          <ChairCategory />
-          // </PrivateRoute>
+          <PrivateRoute>
+            <ChairCategory />
+          </PrivateRoute>
         }
       />
+
+
+      <Route
+        path="/manager/producer"
+        element={
+          <PrivateRoute>
+            <Producer />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/manager/producer/:alias"
+        element={
+          <PrivateRoute>
+            <Producer />
+          </PrivateRoute>
+        }
+      />
+
+
       <Route
         path="/manager/movie"
         element={
---
+++ src/views/pages/Managers/Producer/FilterProducer.jsx
@@ -0,0 +1,59 @@
+import classnames from 'classnames'
+import { useEffect, useState } from 'react'
+// import { Toaster } from 'react-hot-toast'
+import { Link, useNavigate, useParams } from "react-router-dom"
+import { Button, Offcanvas, OffcanvasBody, OffcanvasHeader } from 'reactstrap'
+import FormChairCategory from './FormProducer'
+
+const FilterProducer = (listNameUsed) => {
+  const [canvasPlacement, setCanvasPlacement] = useState('start')
+  const [canvasOpen, setCanvasOpen] = useState(false)
+
+  const navigate = useNavigate()
+  const { alias } = useParams()
+
+  console.log('alias: ', alias);
+
+  useEffect(() => alias && toggleCanvasEnd(), [alias])
+
+  const toggleCanvasStart = () => {
+    navigate("/manager/producer")
+    setCanvasPlacement('start')
+    setCanvasOpen(!canvasOpen)
+  }
+  const toggleCanvasEnd = () => {
+    setCanvasPlacement('end')
+    setCanvasOpen(!canvasOpen)
+  }
+
+  const callbackCanvasOpen = (childData) => {
+    setCanvasOpen(childData)
+  }
+
+  return (
+    <div className='filter-listing'>
+      {/* <Toaster /> */}
+      <div className='btn-wrap'>
+        <div className='demo-inline-spacing'>
+          <Link to={`/manager/producer/add-producer`}>
+            <Button color='primary'>
+              Thêm
+            </Button>
+          </Link>
+          <Offcanvas className='wrap-form w-50' direction={canvasPlacement} isOpen={canvasOpen} toggle={toggleCanvasStart}>
+            <OffcanvasHeader toggle={toggleCanvasStart}>Thêm nhà cung cấp</OffcanvasHeader>
+            <OffcanvasBody
+              className={classnames({
+                'my-auto mx-0 flex-grow-0': canvasPlacement === 'start' || canvasPlacement === 'end'
+              })}
+            >
+              <FormChairCategory parentCallback={callbackCanvasOpen} listNameUsed={listNameUsed} />
+            </OffcanvasBody>
+          </Offcanvas>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default FilterProducer
