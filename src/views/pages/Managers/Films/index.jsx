b0f43d90f1dd804af8e7aa6f54de420d8f9fae35
qnhat <nhatchien12@gmail.com>
2024-10-12 16:00:27 +0700

    update front-end for film manager

--- src/views/pages/Managers/Films/index.jsx
+++ src/views/pages/Managers/Films/index.jsx
@@ -1,12 +1,15 @@
-import React, { Fragment } from 'react'
+import { Fragment } from 'react'
+import '../../../../assets/style/styleSecond.css'
+import '../../../../assets/style/_pagination.scss'
 import TableFilm from './TableFilm'
 
-const Checkout = () => {
-    return (
-        <Fragment>
-            <TableFilm />
-        </Fragment>
-    ) 
-}
+const Checkout = ({ setSelectedKey }) => {
+  setSelectedKey('id_film')
 
+  return (
+    <Fragment>
+      <TableFilm />
+    </Fragment>
+  )
+}
 export default Checkout
